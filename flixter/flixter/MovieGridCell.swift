1b3d77959611dadc5d661891e37b52e27a075a43
selynung <89944133+selynung@users.noreply.github.com>
2021-09-23 19:13:50 -0400

    Added all files

---
+++ flixter/flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Selyn Ung on 9/23/21.
+//  Copyright © 2021 selynung. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
