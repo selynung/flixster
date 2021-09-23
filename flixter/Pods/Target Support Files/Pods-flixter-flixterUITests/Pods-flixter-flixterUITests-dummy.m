cea534023010e75b8af5ad50efaa01084fc87013
selynung <89944133+selynung@users.noreply.github.com>
2021-09-23 19:06:38 -0400

    Delete flixter directory

--- flixter/Pods/Target Support Files/Pods-flixter-flixterUITests/Pods-flixter-flixterUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_flixter_flixterUITests : NSObject
-@end
-@implementation PodsDummy_Pods_flixter_flixterUITests
-@end
--- flixter/Pods/Target Support Files/Pods-flixterTests/Pods-flixterTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_flixterTests : NSObject
-@end
-@implementation PodsDummy_Pods_flixterTests
-@end
